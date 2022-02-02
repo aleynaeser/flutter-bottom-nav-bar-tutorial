93dd16b3536fc9bdfb3d7e5080f0ca9c975f7e9a
Aleyna Eser <aleynaeseer@gmail.com>
2022-02-02 16:28:36 +0300

    first commit

---
+++ style1/lib/screens/settings.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SettingsContent extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Settings Screen"),
+    );
+  }
+}
